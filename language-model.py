cd3a63f5401ec6244216590436c2f260a5ffda2d
Ben Black <bblack4324@gmail.com>
2023-02-14 19:17:45 +0000

    Framework code for python discord API and openAPI

---
+++ language-model.py
@@ -0,0 +1,30 @@
+import discord
+import openai
+import constant
+
+client = discord.Client()
+TOKEN = constant.DISCORDTOKEN
+openai.api_key = constant.OPENAPITOKEN
+model_engine = "ChatGPT"
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!gpt'):
+        prompt = message.content[4:]
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=150,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        await message.channel.send(response.choices[0].text)
+
+client.run(TOKEN)
