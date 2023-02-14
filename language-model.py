afedba7d414e9033ef0a6049040f569bffc1a546
Ben Black <bblack4324@gmail.com>
2023-02-14 20:21:28 +0000

    fleshed out discord feature

--- language-model.py
+++ language-model.py
@@ -1,11 +1,12 @@
 import discord
 import openai
 import constant
+import asyncio # To get the exception
 
-client = discord.Client(intents=discord.Intents.default())
+client = discord.Client(intents=discord.Intents.all())
 TOKEN = constant.DISCORDTOKEN
 openai.api_key = constant.OPENAPITOKEN
-model_engine = "ChatGPT"
+model_engine = "davinci"
 
 @client.event
 async def on_ready():
@@ -16,16 +17,40 @@ async def on_message(message):
     
     if message.author == client.user:
         return
-    if message.content == '!trick':
-        prompt = message.content[4:]
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
+    if message.content == '!begin':
+
+        await message.channel.send("Ask me anything")
+
+        def check(msg):
+            if(msg.author == message.author and msg.channel == message.channel):
+                return msg
+            else:
+                return "Not valid"
+
+        try:
+            msg = await client.wait_for("message", check=check, timeout=30) # 30 seconds to reply
+        except asyncio.TimeoutError:
+            await message.send("Sorry, you didn't reply in time!")
+
+        print("Message: " + message.content)
+
+        response = "Test"
+        
+        response = open_ai_prompt(msg)
+
+
         await message.channel.send(response.choices[0].text)
+
+    
+        def open_ai_prompt(prompt):
+            response = openai.Completion.create(
+                engine=model_engine,
+                prompt=msg,
+                max_tokens=150,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+            return response
 
 client.run(TOKEN)
